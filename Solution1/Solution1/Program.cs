677260e75a26191868cd0a8558b9575a2cd410a5
Nurzhigit <nusmailov@gmail.com>
2017-03-10 10:16:41 +0600

    Solution1

---
+++ Solution1/Solution1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution1
+{
+    class Program
+    {
+        public static void func(){
+            string s = Console.ReadLine();
+            string[] p = s.Split();
+            int[] arr = new int[p.Length];
+            int sum=0;
+            for (int i = 0; i < p.Length; i++)
+            {
+                arr[i] = int.Parse(p[i]);
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            func();
+        }
+    }
+}
