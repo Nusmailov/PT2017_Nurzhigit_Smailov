18eac1f18d89c2d0fef03bd2ce29204e4de482c8
Nurzhigit <nusmailov@gmail.com>
2017-03-10 11:40:51 +0600

    SolutionNew3

--- Solution3/Solution3/Program.cs
+++ Solution3/Solution3/Program.cs
@@ -9,22 +9,26 @@ namespace Solution3
 {
     class Program
     {
-        
-        static void Main(string[] args)
+        static void Show()
         {
             DirectoryInfo directory = new DirectoryInfo(@"G:\KBTU\2semestr\PT\Polygon");
             FileInfo[] files = directory.GetFiles();
-           // DirectoryInfo[] directories = directory.GetDirectories();
+            // DirectoryInfo[] directories = directory.GetDirectories();
             foreach (FileInfo fil in files)
             {
                 Console.WriteLine(fil.Name);
             }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Show();
             Polygon b = new Polygon();
 
-            for (int i = 0; i < 5; i++)
+            /*for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine(b[i].x, body[i].y);
-            }
+            }*/
             Console.ReadKey();
         }
         public void getPerimeter(int x, int y)
